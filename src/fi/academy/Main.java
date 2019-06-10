15450d070adf8093eee53d76f4f82fff473122c2
Heikki Vihemäki <heikki.vihemaki@gmail.com>
2019-06-10 14:25:02 +0300

    From dev

--- src/fi/academy/Main.java
+++ src/fi/academy/Main.java
@@ -2,6 +2,6 @@ package fi.academy;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Dev!");
+        System.out.println("Hello from Dev!");
     }
 }
