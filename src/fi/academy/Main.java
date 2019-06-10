3b282a6e30ce96e1d15e346e7414949d1ae07c24
Heikki Vihemäki <heikki.vihemaki@gmail.com>
2019-06-10 14:30:51 +0300

    well hello hello

--- src/fi/academy/Main.java
+++ src/fi/academy/Main.java
@@ -3,5 +3,7 @@ package fi.academy;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Dev!");
+
+        System.out.println("Well hello hello");
     }
 }
